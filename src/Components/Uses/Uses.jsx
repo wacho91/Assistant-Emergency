f3ac2c47fe7346b70c8d0b134a108a237d6d09fd
wacho91 <criskol.71@gmail.com>
2024-10-14 11:06:05 -0500

    Uses Component Complete

--- src/Components/Uses/Uses.jsx
+++ src/Components/Uses/Uses.jsx
@@ -5,9 +5,42 @@ import Img3 from "../../assets/Uses/3.png"
 
 const Uses = () => {
   return (
-    <div>
-        Uses Component
-    </div>
+    <section>
+        <div className="container my-14">
+            <div className="grid grid-cols-2 xl:grid-cols-4 gap-6">
+                <div className="order-last md:order-first flex flex-col justify-center xl:pr-14">
+                    <h1 className="text-3xl font-bold text-darkBlue">
+                        How it Helps people
+                    </h1>
+                    <p className=" text-gray-400 mt-4">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+                        esse ab natus.
+                    </p>
+                    <p className="text-sm text-gray-400 mt-4">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+                        <a href="#" className="text-primary">
+                        Learn More
+                        </a>
+                    </p>
+                    <button className="w-fit mt-4 border border-gray-200 px-4 py-2 rounded-lg hover:bg-primary hover:text-white transform duration-300">
+                        Get in Touch
+                    </button>
+                </div>
+                <div>
+                    <img src={Img1} alt="" className="w-full rounded-3xl " />
+                    <p className="text-gray-400 mt-4">Coronavirus</p>
+                </div>
+                <div>
+                    <img src={Img2} alt="" className="w-full rounded-3xl" />
+                    <p className="text-gray-400 mt-4">Diagnostic</p>
+                </div>
+                <div>
+                    <img src={Img3} alt="" className="w-full rounded-3xl" />
+                    <p className="text-gray-400 mt-4">Symptoms</p>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 

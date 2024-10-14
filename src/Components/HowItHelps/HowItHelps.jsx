95beb9903d98cc2c2cfd885ddd56042c95876e5e
wacho91 <criskol.71@gmail.com>
2024-10-14 10:38:19 -0500

    HowItComponent Complete

--- src/Components/HowItHelps/HowItHelps.jsx
+++ src/Components/HowItHelps/HowItHelps.jsx
@@ -1,25 +1,59 @@
 
 import Card from "./CardComp.jsx";
-import icon1 from "../../assets/Icon/1 (2).png"
-import icon2 from "../../assets/Icon/2 (2).png"
-import icon3 from "../../assets/Icon/3 (1).png"
+import Icon1 from "../../assets/Icon/1 (2).png"
+import Icon2 from "../../assets/Icon/2 (2).png"
+import Icon3 from "../../assets/Icon/3 (1).png"
 
 const HowItHelps = () => {
   return (
     <section>
-        <div className="conatiner py-16 my-20">
-            <div>
-                <div>
-                    <div>
-                        <Card
-                            icon={icon1}
-                            heading="Symptoms"
-                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risussed volutpat non."
-                        />
-                    </div>
-                </div>
+      <div className="container py-16 my-20">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          <div className="col-span-2">
+            <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-5">
+              <div>
+                <Card
+                  icon={Icon1}
+                  heading="Symptoms"
+                  text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risussed volutpat non."
+                />
+              </div>
+              <div>
+                <Card
+                  icon={Icon2}
+                  heading="Recommendations"
+                  text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risussed volutpat non."
+                />
+              </div>
+              <div>
+                <Card
+                  icon={Icon3}
+                  heading="Local information"
+                  text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risussed volutpat non."
+                />
+              </div>
             </div>
+          </div>
+          <div className="flex flex-col xl:justify-center xl:pr-14">
+            <h1 className="text-3xl font-bold text-darkBlue">
+              How it Helps people
+            </h1>
+            <p className=" text-gray-400 mt-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit
+              esse ab natus.
+            </p>
+            <p className="text-sm text-gray-400 mt-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+              <a href="#" className="text-primary">
+                Learn More
+              </a>
+            </p>
+            <button className="w-fit mt-4 border border-gray-200 px-4 py-2 rounded-lg hover:bg-primary hover:text-white transform duration-300">
+              Get in Touch
+            </button>
+          </div>
         </div>
+      </div>
     </section>
   )
 }

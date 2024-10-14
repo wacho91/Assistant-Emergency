304fdb82e2d78ed2544eb243f10c7d0f70c57902
wacho91 <criskol.71@gmail.com>
2024-10-14 10:08:47 -0500

    CardComp

--- src/Components/HowItHelps/HowItHelps.jsx
+++ src/Components/HowItHelps/HowItHelps.jsx
@@ -1,9 +1,26 @@
 
+import Card from "./CardComp.jsx";
+import icon1 from "../../assets/Icon/1 (2).png"
+import icon2 from "../../assets/Icon/2 (2).png"
+import icon3 from "../../assets/Icon/3 (1).png"
+
 const HowItHelps = () => {
   return (
-    <div>
-        HowItHelps component
-    </div>
+    <section>
+        <div className="conatiner py-16 my-20">
+            <div>
+                <div>
+                    <div>
+                        <Card
+                            icon={icon1}
+                            heading="Symptoms"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risussed volutpat non."
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 

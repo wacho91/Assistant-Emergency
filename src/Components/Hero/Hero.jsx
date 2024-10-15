869a44626a5c8fc995e962495634fa4daa586387
wacho91 <criskol.71@gmail.com>
2024-10-15 10:30:47 -0500

    Animation Hero

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,10 @@
 
 import HeroImage from "../../assets/hero.png"
 
+import { SlideUp } from "../../Utility/Animation"
+import { motion } from "framer-motion"
+
+
 const Hero = () => {
   return (
     <section>
@@ -8,24 +12,44 @@ const Hero = () => {
             {/*Text Section*/}
             <div className="flex flex-col justify-center xl:pr-40">
                 <div className="mt-20 mb-10 md:mt-0 space-y-6 text-center md:text-left">
-                    <h1 className="text-5xl font-bold text-darkBlue">
+                    <motion.h1 
+                        variants={SlideUp(0.2)}
+                        whileInView={"animate"}
+                        initial="initial" 
+                        className="text-5xl font-bold text-darkBlue"
+                    >
                         COVID-19 Risk <br /> Assessment Tool
-                    </h1>
-                    <p className="text-lg text-gray-400 mt-4">
+                    </motion.h1>
+                    <motion.p 
+                        variants={SlideUp(0.4)}
+                        whileInView={"animate"}
+                        initial="initial"
+                        className="text-lg text-gray-400 mt-4"
+                    >
                         A set of solutions designed to help quickly identify coronavirus
                         symptoms and get reliable information regarding COVID-19 concerns.
-                    </p>
-                    <div className="bg-primary text-white px-6 py-4 rounded-lg mt-4 w-fit mx-auto md:mx-0 font-bold hover:shadow-lg duration-200">
+                    </motion.p>
+                    <motion.div 
+                        variants={SlideUp(0.6)}
+                        whileInView={"animate"}
+                        initial="initial"
+                        className="bg-primary text-white px-6 py-4 rounded-lg mt-4 w-fit mx-auto md:mx-0 font-bold hover:shadow-lg duration-200"
+                    >
                         <button>
                             See how to use it
                         </button>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
             {/*Image Section*/}
-            <div className="flex items-start justify-center">
+            <motion.div 
+                initial={{ opacity: 0, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5, delay: 0.6 }} 
+                className="flex items-start justify-center"
+            >
                 <img src={HeroImage} alt="" />
-            </div>
+            </motion.div>
         </div>
     </section>
   )
--- src/Utility/Animation.jsx
+++ src/Utility/Animation.jsx
@@ -1,50 +1,48 @@
 export const SlideUp = (delay) => {
     return {
-        intitial: {
-            y: 100,
-            opcacity: 0
+      initial: {
+        y: 100,
+        opacity: 0,
+      },
+      animate: {
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 0.5,
+          delay,
         },
-        animate: {
-            y: 0,
-            opacity: 1,
-            transition: {
-                duration: 0.5,
-                delay
-            },
-        },
+      },
     };
 };
-
 export const SlideLeft = (delay) => {
     return {
-        intitial: {
-            x: 100,
-            opacity: 0
+      initial: {
+        x: 100,
+        opacity: 0,
+      },
+      animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 0.5,
+          delay,
         },
-        animate: {
-            x:0,
-            opacity: 1,
-            transition: {
-                duration: 0.5,
-                delay
-            },
-        },
+      },
     };
 };
-
-export const SlideRide = (delay) => {
+export const SlideRight = (delay) => {
     return {
-        intitial: {
-            x: -100,
-            opacity: 0
+      initial: {
+        x: -100,
+        opacity: 0,
+      },
+      animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 0.5,
+          delay,
         },
-        animate: {
-            x: 0,
-            opacity: 1,
-            transition: {
-                duration: 0.5,
-                delay
-            },
-        },
+      },
     };
 };

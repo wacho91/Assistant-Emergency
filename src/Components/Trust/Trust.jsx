18a8d8afaba9cad99023c08fa325d893c4d8b8fc
wacho91 <criskol.71@gmail.com>
2024-10-15 10:53:10 -0500

    Animation Trust Component

--- src/Components/Trust/Trust.jsx
+++ src/Components/Trust/Trust.jsx
@@ -1,16 +1,28 @@
 
 import Icon1 from "../../assets/icon1.png"
 import Icon2 from "../../assets/icon2.png"
+import { SlideUp } from "../../Utility/Animation"
+import { motion } from "framer-motion"
 
 const Trust = () => {
   return (
     <section className="bg-brandWhite py-16 mt-20">
         <div className="container">
-            <h1 className="py-6 text-4xl font-bold text-darkBlue text-center">
+            <motion.h1
+                variants={SlideUp(0.2)}
+                initial="initial"
+                whileInView="animate" 
+                className="py-6 text-4xl font-bold text-darkBlue text-center"
+            >
                 Why you can trust this tools
-            </h1>
+            </motion.h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <div className="space-y-6 text-center md:text-left md:px-16 xl:px-32">
+                <motion.div 
+                    variants={SlideUp(0.4)}
+                    initial="initial"
+                    whileInView="animate" 
+                    className="space-y-6 text-center md:text-left md:px-16 xl:px-32"
+                >
                     <img src={Icon1} alt="" className="mx-auto md:mx-0" />
                     <p className=" text-3xl font-semibold">Based on reliable sources</p>
                     <p>
@@ -21,8 +33,13 @@ const Trust = () => {
                         Enchance your preliminary diagnosis and triage with pediatric
                         content <span className="text-primary"> Lear More</span>
                     </p>
-                </div>
-                <div className="space-y-6 text-center md:text-left md:px-16 xl:px-32">
+                </motion.div>
+                <motion.div
+                    variants={SlideUp(0.6)}
+                    initial="initial"
+                    whileInView="animate" 
+                    className="space-y-6 text-center md:text-left md:px-16 xl:px-32"
+                >
                     <img src={Icon2} alt="" className="mx-auto md:mx-0" />
                     <p className="text-3xl font-semibold">Based on reliable sources</p>
                     <p>
@@ -33,7 +50,7 @@ const Trust = () => {
                         Enchance your preliminary diagnosis and triage with pediatric
                         content <span className="text-primary">Learn More</span>
                     </p>
-                </div>
+                </motion.div>
             </div>
         </div>
     </section>

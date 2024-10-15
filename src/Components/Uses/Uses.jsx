5455c5612bd3dc169558934b197c90d9a7a3d43a
wacho91 <criskol.71@gmail.com>
2024-10-15 10:47:37 -0500

    Animation Uses Component

--- src/Components/Uses/Uses.jsx
+++ src/Components/Uses/Uses.jsx
@@ -2,13 +2,20 @@
 import Img1 from "../../assets/Uses/1 (1).png"
 import Img2 from "../../assets/Uses/2 (1).png"
 import Img3 from "../../assets/Uses/3.png"
+import { SlideLeft, SlideRight } from "../../Utility/Animation"
+import { motion } from "framer-motion"
 
 const Uses = () => {
   return (
     <section>
         <div className="container my-14">
             <div className="grid grid-cols-2 xl:grid-cols-4 gap-6">
-                <div className="order-last md:order-first flex flex-col justify-center xl:pr-14">
+                <motion.div 
+                    variants={SlideRight(0.2)}
+                    whileInView={"animate"}
+                    initial="initial" 
+                    className="order-last md:order-first flex flex-col justify-center xl:pr-14"
+                >
                     <h1 className="text-3xl font-bold text-darkBlue">
                         How it Helps people
                     </h1>
@@ -25,19 +32,31 @@ const Uses = () => {
                     <button className="w-fit mt-4 border border-gray-200 px-4 py-2 rounded-lg hover:bg-primary hover:text-white transform duration-300">
                         Get in Touch
                     </button>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div
+                    variants={SlideLeft(0.4)}
+                    whileInView={"animate"}
+                    initial="initial"
+                >
                     <img src={Img1} alt="" className="w-full rounded-3xl " />
                     <p className="text-gray-400 mt-4">Coronavirus</p>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div
+                    variants={SlideLeft(0.6)}
+                    whileInView={"animate"}
+                    initial="initial"
+                >
                     <img src={Img2} alt="" className="w-full rounded-3xl" />
                     <p className="text-gray-400 mt-4">Diagnostic</p>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div
+                    variants={SlideLeft(0.8)}
+                    whileInView={"animate"}
+                    initial="initial"
+                >
                     <img src={Img3} alt="" className="w-full rounded-3xl" />
                     <p className="text-gray-400 mt-4">Symptoms</p>
-                </div>
+                </motion.div>
             </div>
         </div>
     </section>
